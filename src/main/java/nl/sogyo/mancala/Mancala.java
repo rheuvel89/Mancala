8cb37582b2485510b9fa258a2379a4380affc546
Ruurd Heuvel <rheuvel@sogyo.nl>
2019-03-12 17:33:49 +0100

    Changed Mancala class for usage with webapp

--- src/main/java/nl/sogyo/mancala/Mancala.java
+++ src/main/java/nl/sogyo/mancala/Mancala.java
@@ -1,69 +1,40 @@
 package nl.sogyo.mancala;
 
-import java.util.Scanner;
+import java.util.Optional;
 
 public class Mancala {
 
-    private static Scanner in = new Scanner(System.in);
-    private static Player player1 = new Player();
-    private static Bowl bowl1 = new Bowl(player1);
+    private Player player1 = new Player();
+    private Bowl bowl1 = new Bowl(player1);
 
-    public static void main( String[] args ) {
-        while (!bowl1.gameOver()) {
-            doTurn();
-        }
-        endGame();
-    }
 
-    private static void printState() {
-        System.out.println("14|13|12|11|10|9 |8---");
-        System.out.println(getGameState(bowl1));
-        System.out.println("---1 |2 |3 |4 |5 |6 |7");
-
-    }
-
-    private static String getGameState(BaseBowl firstBowl) {
-        String returnString = "";
-        returnString += "   " + firstBowl.getNeighbour(12).seedCount();
-        returnString += "  " + firstBowl.getNeighbour(11).seedCount();
-        returnString += "  " + firstBowl.getNeighbour(10).seedCount();
-        returnString += "  " + firstBowl.getNeighbour(9).seedCount();
-        returnString += "  " + firstBowl.getNeighbour(8).seedCount();
-        returnString += "  " + firstBowl.getNeighbour(7).seedCount();
-        returnString += "  \n";
-        returnString += firstBowl.getNeighbour(13).seedCount() +
-                "                    " +
-                firstBowl.getNeighbour(6).seedCount() + "\n";
-        returnString += "   " + firstBowl.seedCount();
-        returnString += "  " + firstBowl.getNeighbour(1).seedCount();
-        returnString += "  " + firstBowl.getNeighbour(2).seedCount();
-        returnString += "  " + firstBowl.getNeighbour(3).seedCount();
-        returnString += "  " + firstBowl.getNeighbour(4).seedCount();
-        returnString += "  " + firstBowl.getNeighbour(5).seedCount();
-        return returnString;
-    }
-
-    private static int getInput(String prompt) {
-        String input = "";
-        while (input.length() < 1 || !input.matches("[1-9]|1[0-4]")) {
-            System.out.print(prompt);
-            input = in.nextLine();
-        }
-        return Integer.parseInt(input) - 1;
-    }
-
-    private static void doTurn() {
-        printState();
-        BaseBowl turnBowl = bowl1.getNeighbour(getInput(player1.yourTurn() ? "Player 1: " : "Player 2: "));
-        if (turnBowl instanceof Bowl) {
+    public void doTurn(int position) {
+        BaseBowl turnBowl = bowl1.getNeighbour(position);
+        if (turnBowl instanceof Bowl && !bowl1.gameOver()) {
             ((Bowl)turnBowl).doTurn();
         }
     }
 
-    private static void endGame() {
-        bowl1.endGame();
-        bowl1.getWinner();
-        printState();
+    public boolean gameOver() {
+        return bowl1.gameOver();
+    }
+
+    public Optional<Player> getWinner() {
+        if (gameOver()) {
+            bowl1.endGame();
+            Player winner = bowl1.getWinner();
+            return winner != null ? Optional.of(winner) : Optional.empty();
+        }
+        return null;
+    }
+
+    public int getCountOf(int position) {
+        BaseBowl bowl = bowl1.getNeighbour(position);
+        return bowl.seedCount();
+    }
+
+    public Player getActivePlayer() {
+        return player1.yourTurn() ? player1 : player1.getOpponent();
     }
 
 }
--- src/test/java/nl/sogyo/mancala/MancalaTest.java
+++ src/test/java/nl/sogyo/mancala/MancalaTest.java
@@ -3,66 +3,57 @@ package nl.sogyo.mancala;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Optional;
+
 public class MancalaTest {
 
-    /*public static Player player1 = new Player();
-    public static Player player2 = new Player(player1);
-    public static Bowl[] bowls = {new Bowl(player1), new Bowl(player1), new Bowl(player1),
-            new Bowl(player1), new Bowl(player1), new Bowl(player1), new Kalaha(player1),
-            new Bowl(player2), new Bowl(player2), new Bowl(player2),
-            new Bowl(player2), new Bowl(player2), new Bowl(player2), new Kalaha(player2)};
+    public static Mancala mancala;
 
     public void setUp() {
-        bowls[0].linkNeighbour(bowls[1]);
-        bowls[1].linkNeighbour(bowls[2]);
-        bowls[2].linkNeighbour(bowls[3]);
-        bowls[3].linkNeighbour(bowls[4]);
-        bowls[4].linkNeighbour(bowls[5]);
-        bowls[5].linkNeighbour(bowls[6]);
-        bowls[6].linkNeighbour(bowls[7]);
-        bowls[7].linkNeighbour(bowls[8]);
-        bowls[8].linkNeighbour(bowls[9]);
-        bowls[9].linkNeighbour(bowls[10]);
-        bowls[10].linkNeighbour(bowls[11]);
-        bowls[11].linkNeighbour(bowls[12]);
-        bowls[12].linkNeighbour(bowls[13]);
-        bowls[13].linkNeighbour(bowls[0]);
+        mancala = new Mancala();
     }
 
     public void tearDown() {
-        player1 = new Player();
-        player2 = new Player(player1);
-        bowls = new Bowl[] {new Bowl(player1), new Bowl(player1), new Bowl(player1),
-                new Bowl(player1), new Bowl(player1), new Bowl(player1), new Kalaha(player1),
-                new Bowl(player2), new Bowl(player2), new Bowl(player2),
-                new Bowl(player2), new Bowl(player2), new Bowl(player2), new Kalaha(player2)};
+        mancala = null;
     }
 
     @Test
-    public void testApp() {
+    public void testDoTurnOnBowlInActiveGame() {
         setUp();
+        mancala.doTurn(0);
 
-        bowls[4].doTurn();
-        bowls[7].doTurn();
-        bowls[0].doTurn();
+        int bowl1Count = mancala.getCountOf(0);
 
-        Assert.assertFalse(player1.yourTurn());
-        Assert.assertTrue(player2.yourTurn());
-        Assert.assertEquals(0, bowls[0].seedCount());
-        Assert.assertEquals(5, bowls[1].seedCount());
-        Assert.assertEquals(5, bowls[2].seedCount());
-        Assert.assertEquals(5, bowls[3].seedCount());
-        Assert.assertEquals(0, bowls[4].seedCount());
-        Assert.assertEquals(5, bowls[5].seedCount());
-        Assert.assertEquals(8, bowls[6].seedCount());
-        Assert.assertEquals(0, bowls[7].seedCount());
-        Assert.assertEquals(0, bowls[8].seedCount());
-        Assert.assertEquals(5, bowls[9].seedCount());
-        Assert.assertEquals(5, bowls[10].seedCount());
-        Assert.assertEquals(5, bowls[11].seedCount());
-        Assert.assertEquals(5, bowls[12].seedCount());
-        Assert.assertEquals(0, bowls[13].seedCount());
+        Assert.assertEquals(0, bowl1Count);
 
         tearDown();
-    }*/
+    }
+
+    @Test
+    public void testDoTurnOnKalahaInActiveGame() {
+        setUp();
+        mancala.doTurn(6);
+
+        int bowl6Count = mancala.getCountOf(5);
+        int bowl7Count = mancala.getCountOf(6);
+        int bowl8Count = mancala.getCountOf(7);
+
+        Assert.assertEquals(4, bowl6Count);
+        Assert.assertEquals(0, bowl7Count);
+        Assert.assertEquals(4, bowl8Count);
+
+        tearDown();
+    }
+
+    @Test
+    public void testGetWinnerInActiveGame() {
+        setUp();
+
+        Optional winner = mancala.getWinner();
+
+        Assert.assertEquals(null, winner);
+
+        tearDown();
+    }
+
 }
